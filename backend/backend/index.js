4527b620378f9223fb7a7cb6f879b2d504d15b33
megapowersolucoes <contato@megapowersolucoes.com.br>
2025-10-16 14:18:08 -0300

    Delete backend directory

--- backend/backend/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const app = express();
-const PORT = process.env.PORT || 8080;
-
-app.get("/health", (req, res) => res.json({ status: "OK", service: "Megapower Core" }));
-
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor Megapower Core rodando na porta ${PORT}`);
-});
