50e8acf59c33745be4c92756486ade6cc8424280
megapowersolucoes <contato@megapowersolucoes.com.br>
2025-10-17 22:02:12 -0300

    Create firebaseConfig.js for Firebase setup
    Add Firebase configuration and initialization.

---
+++ pages/core/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDbZuoAxpA-jRa7Kxn506DqeqzDwKPa-c",
+  authDomain: "megapower-solucoes.firebaseapp.com",
+  projectId: "megapower-solucoes",
+  storageBucket: "megapower-solucoes.firebasestorage.app",
+  messagingSenderId: "64488455601",
+  appId: "1:64488455601:web:a45d66e38feda8f966d190"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
